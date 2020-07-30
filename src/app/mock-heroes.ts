befbafdc66557b6b34576eb5dc428cc713d8eb68
A768251 <a768251@atos.net>
2020-07-30 14:37:54 +0200

    Tour of Heroes - premier commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Loki' },
+  { id: 12, name: 'Dr Strange' },
+  { id: 13, name: 'Iron Man' },
+  { id: 14, name: 'Captain America' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Spider Man' },
+  { id: 17, name: 'Dynamax' },
+  { id: 18, name: 'Dr Stevenson' },
+  { id: 19, name: 'Magmar' },
+  { id: 20, name: 'Tornade' }
+];
